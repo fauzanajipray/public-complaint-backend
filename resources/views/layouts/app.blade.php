afab3a092dabd504a1b78f9b4e1f182f72b5cf52
faprayyy <fauzanjr1@gmail.com>
2022-01-24 12:42:42 +0700

    add fontawsome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,12 +9,11 @@
     <meta name="author" content="Fauzan Aji Prayoga">
     
     <title>Laravel</title>
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
-    <!-- Bootstrap CSS -->
+        
     <link href="{{ asset("css/app.css") }}" rel="stylesheet" >
     @yield('css')
 
