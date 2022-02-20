60e9742516910fde9589992a6c751fe363e23fb8
faprayyy <fauzanjr1@gmail.com>
2022-02-20 16:19:11 +0700

    change user_id table comment  ondelete cascade

--- database/migrations/2022_01_27_203458_create_comments_table.php
+++ database/migrations/2022_01_27_203458_create_comments_table.php
@@ -15,7 +15,7 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('complaint_id')->constrained()->onDelete('cascade');
             $table->text('body');
             $table->enum('status', ['Menunggu', 'Diteruskan', 'Diterima', 'Ditolak'])->default('Menunggu');
